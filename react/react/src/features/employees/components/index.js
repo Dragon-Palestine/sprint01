9463122b455dd35c72ab34a6a7d34421505d5751
Dragon <167077773+Dragon-Palestine@users.noreply.github.com>
2026-04-01 11:21:42 +0200

    fix: remove duplicate react and public folders from git tracking

--- react/react/src/features/employees/components/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Employee Components Module
- * Central export for all employee-related components
- */
-
-export { default as SearchBar } from "./SearchBar";
-export { default as FilterPanel } from "./FilterPanel";
-export { default as EmployeeCard } from "./EmployeeCard";
-export { default as EmployeeList } from "./EmployeeList";
